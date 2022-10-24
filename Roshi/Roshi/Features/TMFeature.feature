393f06553badc8ad0b38bdf2dfba4cf12e1bd5d8
roshiena <roshina@hotmail.com>
2022-10-24 13:59:37 +0800

    Specflow Implementation

---
+++ Roshi/Roshi/Features/TMFeature.feature
@@ -0,0 +1,12 @@
+﻿Feature: TMFeature
+
+As a TurnUp Admin
+I would like to create, edit and delete Time and Management record
+So that I can manage employee's time and management record
+
+@tag1
+Scenario: [Create Time and Management Record]
+	Given [I logged into TurnUp Portal successfully]
+	When [I navigate to Time and Material Page]
+	And [Create a new time and management record]
+	Then [A new record is created successfully]
---
+++ Roshi/Roshi/StepDefinitions/TMFeatureStepDefinitions.cs
@@ -0,0 +1,33 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace Roshi.StepDefinitions
+{
+    [Binding]
+    public class TMFeatureStepDefinitions
+    {
+        [Given(@"\[I logged into TurnUp Portal successfully]")]
+        public void GivenILoggedIntoTurnUpPortalSuccessfully()
+        {
+           
+        }
+
+        [When(@"\[I navigate to Time and Material Page]")]
+        public void WhenINavigateToTimeAndMaterialPage()
+        {
+            
+        }
+
+        [When(@"\[Create a new time and management record]")]
+        public void WhenCreateANewTimeAndManagementRecord()
+        {
+           
+        }
+
+        [Then(@"\[A new record is created successfully]")]
+        public void ThenANewRecordIsCreatedSuccessfully()
+        {
+           
+        }
+    }
+}
